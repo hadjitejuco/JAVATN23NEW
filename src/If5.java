78699d9289a38640e0008d1ededb74f893abf840
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-09-08 08:47:09 +0800

    first commit

---
+++ src/If5.java
@@ -0,0 +1,13 @@
+//nested if statements
+
+public class If5 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 30;
+        if (y==20){
+            if (x == 10){
+                System.out.println("value of x is 10 and y is 20");
+            }
+        }
+    }
+}
