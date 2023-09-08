034eb6c06e5154ead5b7dc7eefab8a339ae848fb
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-09-08 08:29:49 +0800

    first commit

--- src/If1.java
+++ src/If1.java
@@ -1,4 +1,24 @@
 //https://github.com/hadjitejuco/JAVATN23NEW
 public class If1 {
-    
+    public static void main(String[] args) {
+        //if-then statement
+        /*
+            if (condition){
+                statements  // true   // false
+            }
+        */
+        
+        //determining if a number is positive and negative
+        // number < 0 
+        
+        int x  = 10;
+        int y = -1;
+        if (x > 0){
+            System.out.println("Number is positive");
+        }
+        if (y < 0){
+            System.out.println("Number is negative");
+        }
+        System.out.println("End..."); // out of block
+    }
 }
