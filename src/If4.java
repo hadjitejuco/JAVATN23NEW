f9d79a403017743a9ebe34763dadd5618b93e854
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-09-08 08:41:41 +0800

    first commit

---
+++ src/If4.java
@@ -0,0 +1,17 @@
+//if else if statement
+//write multiple conditions
+public class If4 {
+    public static void main(String[] args) {
+        //positive > 0, negative < 0, 0 -> the number is 0
+        int x = -10;
+        
+        if (x > 0){
+            System.out.println("positive");
+        }else if (x < 0){
+            System.out.println("negative");
+        }else{
+            System.out.println("Number is zero");
+        }
+    }
+ 
+}
