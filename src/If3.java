9f36965429a9d01ab701daa1d3238eaf61583bcf
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-09-08 08:37:33 +0800

    first commit

---
+++ src/If3.java
@@ -0,0 +1,13 @@
+// if else statement
+public class If3 {
+    public static void main(String[] args) {
+        //to test if a number is positive or negative
+        //int x  = 10;
+        int x  = -10;
+        if (x > 0){
+            System.out.println("The number is positive");
+        }else{
+            System.out.println("The number is negative");
+        }
+    }
+}
