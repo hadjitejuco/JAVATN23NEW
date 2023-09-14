f3ea73099a2a65a3561886258a94051738da8c30
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-09-15 07:16:00 +0800

    first commit

---
+++ src/For1.java
@@ -0,0 +1,11 @@
+//simple for loop
+public class For1 {
+    public static void main(String[] args) {
+          //   1         2      4
+        for (int i = 0; i < 5; i++) {
+            //code to execute -> true 
+            System.out.println("Loop: "+i);  //3
+        }
+        System.out.println("Next statement");
+    }
+}
