8b1364019a433526d6cc83c8b60febbb3ab5c1b1
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-09-15 09:35:26 +0800

    first commit

---
+++ src/Array7.java
@@ -0,0 +1,8 @@
+
+public class Array7 {
+    //Enter the size of the array
+    //Enter the values
+    //Enter a number from the list
+        //get the number of occurances of that number and display 
+    
+}
