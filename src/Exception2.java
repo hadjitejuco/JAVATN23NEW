6cac974017705a8748866be5c6fa48b80db3c321
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-10-20 07:37:57 +0800

    first commit

--- src/Exception2.java
+++ src/Exception2.java
@@ -9,4 +9,21 @@
  */
 public class Exception2 {
     
+    public static void main(String[] args) {
+        int d,a;
+        try {
+            d = 0;
+            a =10/d;
+            System.out.println(a);
+            System.out.println("This will not be printed");
+        
+        }
+        catch(ArithmeticException e){
+            System.out.println("Divsion by Zero, Arithmetic Exception");
+        
+        }
+    }
+    
+    
+    
 }
