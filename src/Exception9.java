98d58515a2cd472eb14118f2fe2f8016bcbabf41
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-10-20 12:37:50 +0800

    first commit

---
+++ src/Exception9.java
@@ -0,0 +1,12 @@
+//-ea 
+public class Exception9 {
+    public static void main(String args[]) {
+        int age = 1;
+        assert(age>0);
+        /* if age is valid (i.e., age>0) */
+        if (age >= 18) {
+        System.out.println("Congrats! You're an adult! =)");
+        }
+    }
+    
+}
