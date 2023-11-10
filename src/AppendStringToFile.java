e7d619d0de03f67a4bcc624ceda83c7f90d8ae62
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-11-10 08:20:28 +0800

    first commit

---
+++ src/AppendStringToFile.java
@@ -0,0 +1,37 @@
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class AppendStringToFile {
+      public static void main(String[] args) {
+         Scanner scanner = new Scanner (System.in);
+        try {
+            System.out.println("Enter a string to write to the file: ");
+            String userInput = scanner.nextLine();
+           
+            File myFile = new File ("/Users/hadjitejuco/NetBeansProjects/TN23/src/sample.txt");
+            
+            //append mode
+            FileWriter writter = new FileWriter(myFile, true);
+            
+            //Write character
+            writter.write(userInput + "\n");
+            
+            writter.close();
+            
+            System.out.println("Successfully written data to file...");
+
+        }catch(IOException e){
+             System.err.println("An Error occured ");
+             e.printStackTrace(); //handle the exception error pinpoint line erro
+        }finally{
+            scanner.close();
+        
+        }  
+        
+    }
+}
+
